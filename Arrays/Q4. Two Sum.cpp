52708bf0795412e8d36925c44f246fd9cec99ef3
Sakib2186 <skmdsakib2186@gmail.com>
2026-01-01 22:58:23 +0600

    Arrays: Q4.cpp

---
+++ Arrays/Q4. Two Sum.cpp
@@ -0,0 +1,57 @@
+/*
+Problem Statement:
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+Problem Type: Easy
+
+Problem Link: https://leetcode.com/problems/two-sum/description/
+
+
+*/
+#include <map>;
+#include <vector>;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        map<int,int> items;
+        vector<int> result;
+        for(int i=0; i<nums.size();i++)
+        {
+            int remainder = target-nums[i];
+            if(items.contains(remainder))
+            {
+                result.push_back(items[remainder]);
+                result.push_back(i);
+            }
+            else
+            {
+                items.insert({nums[i],i});
+            }
+        }
+        return result;
+        
+    }
+};
