c83a1b3a59107f56f1a8d7dea3d4de43f7d9f13f
Sakib2186 <skmdsakib2186@gmail.com>
2026-02-02 21:36:17 +0600

    HashMap: Q2

---
+++ HashMap/Q2. First Missing Positive.py
@@ -0,0 +1,56 @@
+'''
+
+Problem Statement:
+
+
+Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+
+You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,0]
+Output: 3
+Explanation: The numbers in the range [1,2] are all in the array.
+Example 2:
+
+Input: nums = [3,4,-1,1]
+Output: 2
+Explanation: 1 is in the array but 2 is missing.
+Example 3:
+
+Input: nums = [7,8,9,11,12]
+Output: 1
+Explanation: The smallest positive integer 1 is missing.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+
+
+Problem Type: Hard
+
+Problem Link: https://leetcode.com/problems/first-missing-positive/
+
+
+'''
+import heapq
+class Solution:
+    def firstMissingPositive(self, nums):
+        hash_table = {}
+        max_heap = []
+        for i in nums:
+            hash_table[i] = 1
+            heapq.heappush(max_heap,-i)
+        max_value = max(-max_heap[0],0)
+        for i in range(1,max_value+1):
+            if i in hash_table:
+                continue
+            else:
+                return i
+        return max_value + 1
+        
