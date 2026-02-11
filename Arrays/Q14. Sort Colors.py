7b6edf4d67ef7f06c943caeb94cdf740a3fc81c3
Sakib2186 <skmdsakib2186@gmail.com>
2026-02-11 20:16:16 +0600

    Arrays: Q14

--- Arrays/Q14. Sort Colors.py
+++ Arrays/Q14. Sort Colors.py
@@ -42,3 +42,5 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         nums[:] = [0]*nums.count(0) + [1]*nums.count(1) + [2]*nums.count(2)
+
+# could be solved efficiently
